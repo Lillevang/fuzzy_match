482f83f890ef1595790adc8120e1c723eef08235
Lillevang <jeppe.lillevang@gmail.com>
2025-02-28 00:08:53 +0100

    Set up scaffold for module

--- src/fuzzy_match.cr
+++ src/fuzzy_match.cr
@@ -1,6 +1,25 @@
 # TODO: Write documentation for `FuzzyMatch`
+
+require "levenshtein"
+
 module FuzzyMatch
   VERSION = "0.1.0"
 
   # TODO: Put your code here
+
+  def self.ratio(s1: String, s2: String) : Float64
+  end
+
+  def self.partial_ratio(s1: String, s2: String) : Float64
+  end
+
+  def self.token_sort_ratio(s1: String, s2: String) : Float64
+  end
+
+  def self.token_set_ratio(s1: String, s2: String) : Float64
+  end
+
+  def self.normalize(s : String) : String
+  end
+  
 end
